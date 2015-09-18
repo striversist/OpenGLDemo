1fa73f8456c6bbfddd83f7b30b475ee82410b5ae
aarontang <aarontang@tencent.com>
2015-09-18 16:24:12 +0800

    增加注释

--- src/com/example/opengldemo/MyGLRenderer.java
+++ src/com/example/opengldemo/MyGLRenderer.java
@@ -27,6 +27,19 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         gl.glMatrixMode(GL10.GL_PROJECTION);        // set matrix to projection mode
         gl.glLoadIdentity();                        // reset the matrix to its default state
         // 把y坐标跨度定为(-1,1)，那么相应x坐标跨度为(-ratio,ratio)。这将OpenGL坐标和屏幕坐标比联系起来
+        /**
+         * 截取场景
+         * void glFrustum(GLdouble left,
+         *   GLdouble right,
+         *   GLdouble bottom,
+         *   GLdouble top,
+         *   GLdouble nearVal,
+         *   GLdouble farVal)
+         *   
+         *   left, right: Specify the coordinates for the left and right vertical clipping planes.
+         *   bottom, top: Specify the coordinates for the bottom and top horizontal clipping planes.
+         *   nearVal, farVal: Specify the distances to the near and far depth clipping planes. Both distances must be positive.
+         */
         gl.glFrustumf(-ratio, ratio, -1, 1, 3, 7);  // apply the projection matrix
 //        gl.glOrthof(-ratio, ratio, -1, 1, 3, 7);
     }
@@ -41,7 +54,7 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
         // When using GL_MODELVIEW, you must set the camera view
         // 把自己的眼睛当成是照相机，前三个参数表示眼睛的坐标，中间三个参数表示要拍照的物体的中心位置，可以理解成焦点吧，
         // 后三个参数表示头顶的朝向
-        GLU.gluLookAt(gl, 0, 0, -5, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
+        GLU.gluLookAt(gl, 0, 0, -5, 0f, 0f, 0f, 0.0f, 1.0f, 0.0f);
         
 //        mTriangle.draw(gl);
         mSquare.draw(gl);
