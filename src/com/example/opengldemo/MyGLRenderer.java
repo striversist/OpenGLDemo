753b1cb0cbfeb43d7bf1c7bcd588b7b12d502ef9
aarontang <aarontang@tencent.com>
2015-09-18 16:08:57 +0800

    不会变形的正方形

--- src/com/example/opengldemo/MyGLRenderer.java
+++ src/com/example/opengldemo/MyGLRenderer.java
@@ -4,6 +4,7 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import android.opengl.GLSurfaceView;
+import android.opengl.GLU;
 
 public class MyGLRenderer implements GLSurfaceView.Renderer {
 
@@ -19,13 +20,28 @@ public class MyGLRenderer implements GLSurfaceView.Renderer {
     
     @Override
     public void onSurfaceChanged(GL10 gl, int width, int height) {
-        
+        gl.glViewport(0, 0, width, height);
+
+        // make adjustments for screen ratio
+        float ratio = (float) width / height;
+        gl.glMatrixMode(GL10.GL_PROJECTION);        // set matrix to projection mode
+        gl.glLoadIdentity();                        // reset the matrix to its default state
+        // 把y坐标跨度定为(-1,1)，那么相应x坐标跨度为(-ratio,ratio)。这将OpenGL坐标和屏幕坐标比联系起来
+        gl.glFrustumf(-ratio, ratio, -1, 1, 3, 7);  // apply the projection matrix
+//        gl.glOrthof(-ratio, ratio, -1, 1, 3, 7);
     }
 
     @Override
     public void onDrawFrame(GL10 gl) {
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
-        gl.glLoadIdentity();
+        // Set GL_MODELVIEW transformation mode
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        gl.glLoadIdentity();                      // reset the matrix to its default state
+
+        // When using GL_MODELVIEW, you must set the camera view
+        // 把自己的眼睛当成是照相机，前三个参数表示眼睛的坐标，中间三个参数表示要拍照的物体的中心位置，可以理解成焦点吧，
+        // 后三个参数表示头顶的朝向
+        GLU.gluLookAt(gl, 0, 0, -5, 0f, 0f, 0f, 0f, 1.0f, 0.0f);
         
 //        mTriangle.draw(gl);
         mSquare.draw(gl);
